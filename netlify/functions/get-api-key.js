d168a4eec7cfcba2916558ef16d5e39575e87a96
--unset <vicky@tito.io>
2025-01-13 13:00:28 +0000

    Include custom domain

--- netlify/functions/get-api-key.js
+++ netlify/functions/get-api-key.js
@@ -1,19 +1,22 @@
 const fetch = require('node-fetch');
 
-exports.handler = async function () {
+exports.handler = async function (event) {
     const SHEET_ID = '1ETd8ZgatNS7e6_3RkqBF2gzQMsJ5UYpWU9pVJ862cGI';
     const RANGE = 'Sheet1!A:F';
     const API_KEY = process.env.GOOGLE_API_KEY;
+
+    const referer = event.headers.referer || 'https://quovixi.com/';
 
     try {
         const response = await fetch(
             `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`,
             {
                 headers: {
-                    Referer: 'https://your-site-domain/', // Replace with your actual site domain
+                    Referer: referer,
                 },
             }
         );
+
         const data = await response.json();
 
         return {
